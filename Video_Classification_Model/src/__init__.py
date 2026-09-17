a794a6c067b20c90067363e3bdc773e0a3d346de
naoromi <bryanzhang0601@gmail.com>
2026-09-17 18:11:40 +0800

    Delete legacy files again.

--- Video_Classification_Model/src/__init__.py
+++ Video_Classification_Model/src/__init__.py
@@ -1,13 +1,1 @@
-"""Video classification data utilities and legacy PyTorch helpers."""
-
-from .collate import collate_into_matrix
-from .eval_pt import load_and_test_model
-from .extract import extract_covers
-from .train_pt import train_model
-
-__all__ = [
-    "extract_covers",
-    "collate_into_matrix",
-    "train_model",
-    "load_and_test_model",
-]
+"""Video classification package."""
